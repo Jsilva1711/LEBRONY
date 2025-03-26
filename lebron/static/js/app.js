1db1392485e6c3068b3b39da57bf3630054d9107
DSpiker <dspiker23@gmail.com>
2025-03-26 16:58:46 -0500

    part 1 done

--- lebron/static/js/app.js
+++ lebron/static/js/app.js
@@ -16,43 +16,54 @@ function reloadPosts() {
     .then(posts => {
         let postsElm = document.getElementById('posts');
         postsElm.replaceChildren();
+	
+        const pathParts = window.location.pathname.split('/');
+        const profileUserId = pathParts[2];
 
-        posts.forEach( (post) => {
+        let filteredPosts = posts;
+
+        if (!isNaN(profileUserId)) {
+            const profileIdInt = parseInt(profileUserId, 10);
+            filteredPosts = posts.filter(post => post.profile.id === profileIdInt);
+        }
+
+        filteredPosts.forEach(post => {
             let pElm = document.createElement('p');
             pElm.textContent = post.content + " by ";
             pElm.append(profileLink(post.profile));
             postsElm.append(pElm);
-        } );
+        });
     })
     .catch(error => showError(error));
 }
-
-function showError(error) {
-    console.log(error);
-}
-
+	
+	
+       
 function load() {
-    document
-        .getElementById('post-btn')
-        .addEventListener('click', (evt) => {
-            evt.preventDefault();
+	const element = document.getElementById('post-btn');
+	if (element) {
+		document
+        		.getElementById('post-btn')
+        		.addEventListener('click', (evt) => {
+            		evt.preventDefault();
 
-            let content = document.getElementById('content').value;        
+            		let content = document.getElementById('content').value;        
 
-            fetch('/api/post', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ content: content })
-            })
-            .then(response => response.json())
-            .then(data => reloadPosts())
-            .catch(error => showError(error));
-        });
-
-    // Get the current posts.
-    reloadPosts();
+            		fetch('/api/post', {
+                		method: 'POST',
+                		headers: {
+                    		'Content-Type': 'application/json'
+                		},
+                		body: JSON.stringify({ content: content })
+            		})
+            		.then(response => response.json())
+            		.then(data => reloadPosts())
+            		.catch(error => showError(error));
+        		});
+			}
+	
+    		// Get the current posts.
+    		reloadPosts();
 }
 
 window.onload = load;
